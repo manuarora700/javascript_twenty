3be495579e6817a2b2fe644a658cc92221ed1a14
Manu Arora <mannuarora700@gmail.com>
2020-02-28 14:22:18 +0530

    Swapping done

--- 19SortableDragDropList/script.js
+++ 19SortableDragDropList/script.js
@@ -48,4 +48,61 @@ function createList() {
 
       draggable_list.appendChild(listItem);
     });
+
+  addEventListeners();
+}
+
+function dragStart() {
+  //   console.log("Event", "dragStart");
+  dragStartIndex = +this.closest("li").getAttribute("data-index");
+  //   console.log(dragStartIndex);
+}
+
+function dragEnter() {
+  //   console.log("Event", "dragEnter");
+  this.classList.add("over");
+}
+
+function dragLeave() {
+  //   console.log("Event", "dragLeave");
+  this.classList.remove("over");
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  //   console.log("Event", "dragOver");
+}
+
+function dragDrop() {
+  //   console.log("Event", "drop");
+  const dragEndIndex = +this.getAttribute("data-index");
+
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove("over");
+}
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector(".draggable");
+  const itemTwo = listItems[toIndex].querySelector(".draggable");
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+
+  //   console.log(itemOne, itemTwo);
+}
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll(".draggable");
+  const dragListItems = document.querySelectorAll(".draggable-list li");
+
+  draggables.forEach(draggable => {
+    draggable.addEventListener("dragstart", dragStart);
+  });
+  dragListItems.forEach(item => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("drop", dragDrop);
+    item.addEventListener("dragenter", dragEnter);
+    item.addEventListener("dragleave", dragLeave);
+  });
 }
